704ca11a5f5f732e8a5e4a63fde0d9112459fe4f
jinsu-dain <sjs0270@daum.net>
2019-12-21 16:30:40 +0900

    5
    5

---
+++ insert.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+$conn = new mysqli('sjs0274.cafe24.com', 'sjs0274', 'tlawlstn12146212', 'insert');
+$inputName=$_POST['inputName'];
+$inputMessage=$_POST['inputMessage'];
+$inputNumber=$_POST['inputNumber'];
+$sql="INSERT INTO `data` (`id`, `inputName`, `inputMessage`, `inputNumber`) VALUES (NULL, '$inputName', '$inputMessage', '$inputNumber')";
+if ($conn->query($sql) === TRUE) {
+    echo "data inserted";
+}
+else 
+{
+    echo "failed";
+}
+?>
+</body>
+</html>
